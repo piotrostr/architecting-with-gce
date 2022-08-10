3fe979a63b391920aec367115b11e3d4908822ec
Piotr Ostrowski <piotrostr@google.com>
2022-08-10 18:31:05 +0100

    feat: add common network for the instances, finish the lab

--- foundation/vpc-networking/main.tf
+++ foundation/vpc-networking/main.tf
@@ -1,3 +1,22 @@
+resource "google_compute_network" "mynetwork" {
+  name = "mynetwork"
+}
+
+resource "google_compute_firewall" "mynetwork" {
+  name = "web"
+  network = google_compute_network.mynetwork.name
+  source_tags = ["web"]
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    ports = [ "80", "22", "443" ]
+    protocol = "tcp"
+  }
+}
+
 resource "google_compute_instance" "mynet-us-vm" {
   name         = "mynet-us-vm"
   machine_type = "n1-standard-1"
@@ -5,7 +24,7 @@ resource "google_compute_instance" "mynet-us-vm" {
   can_ip_forward = true
 
   network_interface {
-    network = "default"
+    network = google_compute_network.mynetwork.name
   }
 
   boot_disk {
@@ -22,7 +41,7 @@ resource "google_compute_instance" "mynet-eu-vm" {
   can_ip_forward = true
 
   network_interface {
-    network = "default"
+    network = google_compute_network.mynetwork.name
   }
 
   boot_disk {
