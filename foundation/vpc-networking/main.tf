5d36e8e87e748fc9fb5d3a45912f2d6abd1ee725
Piotr Ostrowski <piotrostr@google.com>
2022-08-10 18:12:05 +0100

    feat: duplicate the instance in another region

--- foundation/vpc-networking/main.tf
+++ foundation/vpc-networking/main.tf
@@ -3,7 +3,24 @@ resource "google_compute_instance" "mynet-us-vm" {
   machine_type = "n1-standard-1"
   zone         = "us-central1-c"
   can_ip_forward = true
-  tags         = ["mynet-us-vm"]
+
+  network_interface {
+    network = "default"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-10-buster-v20220719"
+    }
+  }
+}
+
+resource "google_compute_instance" "mynet-eu-vm" {
+  name         = "mynet-eu-vm"
+  machine_type = "n1-standard-1"
+  zone         = "europe-west1-c"
+  can_ip_forward = true
+
   network_interface {
     network = "default"
   }
