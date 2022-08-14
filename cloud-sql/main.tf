d1e323eab0a9a6e7dc87699236683e2ce0de8790
Piotr Ostrowski <piotrostr@google.com>
2022-08-14 18:55:53 +0100

    feat: add static external IPs, configure NAT

--- cloud-sql/main.tf
+++ cloud-sql/main.tf
@@ -19,11 +19,27 @@ resource "google_compute_subnetwork" "main_instance_subnetwork" {
   // https://cloud.google.com/vpc/docs/subnets#ip-ranges
 }
 
+// allocate a static IP for each of the instances
+resource "google_compute_address" "static_public_ip_us" {
+  name = "static-public-ip-us"
+  region = "us-central1"
+  address_type = "EXTERNAL"
+}
+
+// cool thing is that the addresses can be defined in the subnetwork
+// need to match the CIDR, but a lot of flexibility
+resource "google_compute_address" "static_public_ip_eu" {
+  name = "static-public-ip-eu"
+  region = "europe-west1"
+  address_type = "EXTERNAL" // or "INTERNAL", and specify the subnetwork
+}
+
 resource "google_compute_firewall" "web_rules" {
   project     = var.project
   name        = "web-rules"
   network     = google_compute_network.vpc.name
-  description = "Creates firewall rule targeting tagged instances"
+  description = "Creates INGRESS firewall rule targeting tagged instances"
+  source_ranges = [ "0.0.0.0/0" ]
 
   // allow pings as well
   allow {
@@ -52,10 +68,20 @@ resource "google_compute_instance" "wordpress_instance_eu" {
   network_interface {
     subnetwork = google_compute_subnetwork.proxy_instance_subnetwork.name
 
-    // include the following to get a public IP address
+    // block below can be left empty
+    // (purpose of the block is to allow EGRESS)
+    //
+    // since the wordpress instances
+    // will sit behind a domain likely, lets assign static IPs
+    access_config {
+      nat_ip = google_compute_address.static_public_ip_eu.address
+    }
+
+    // include the following to also get a public IPv6 address
     ipv6_access_config {
       network_tier = "PREMIUM"
     }
+
   }
 
   boot_disk {
@@ -73,6 +99,10 @@ resource "google_compute_instance" "wordpress_instance_us" {
 
   network_interface {
     subnetwork = google_compute_subnetwork.main_instance_subnetwork.name
+
+    access_config {
+      nat_ip = google_compute_address.static_public_ip_us.address
+    }
   }
 
   boot_disk {
