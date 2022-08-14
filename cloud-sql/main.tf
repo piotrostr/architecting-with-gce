107a6201be38037711388389fac8b28fea1a8c05
Piotr Ostrowski <piotrostr@google.com>
2022-08-14 18:33:51 +0100

    feat: add external traffic for the proxy instance, add web-firewall

--- cloud-sql/main.tf
+++ cloud-sql/main.tf
@@ -8,19 +8,36 @@ resource "google_compute_subnetwork" "proxy_instance_subnetwork" {
   name          = "proxy-instance-subnetwork"
   network       = google_compute_network.vpc.name
   region        = "europe-west1"
-  ip_cidr_range = "10.132.0.0/20" # the CIDRs are predefined per region!
+  ip_cidr_range = "10.132.0.0/20" // the CIDRs are predefined per region!
 }
 
 resource "google_compute_subnetwork" "main_instance_subnetwork" {
   name          = "main-instance-subnetwork"
   network       = google_compute_network.vpc.name
   region        = "us-central1"
-  ip_cidr_range = "10.128.0.0/20" # all CIDRs can be found below
-  # https://cloud.google.com/vpc/docs/subnets#ip-ranges
+  ip_cidr_range = "10.128.0.0/20" // all CIDRs can be found below
+  // https://cloud.google.com/vpc/docs/subnets#ip-ranges
 }
 
+resource "google_compute_firewall" "web_rules" {
+  project     = var.project
+  name        = "web-rules"
+  network     = google_compute_network.vpc.name
+  description = "Creates firewall rule targeting tagged instances"
 
-# instance stuff
+  // allow pings as well
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "8080", "443"]
+  }
+  target_tags = ["web"]
+}
+
+// instance stuff
 data "google_compute_image" "debian" {
   project = "debian-cloud"
   family  = "debian-11"
@@ -30,9 +47,15 @@ resource "google_compute_instance" "wordpress_instance_eu" {
   name         = "wordpress-instance-eu"
   machine_type = "e2-small"
   zone         = "europe-west1-b"
+  tags         = ["web"]
 
   network_interface {
     subnetwork = google_compute_subnetwork.proxy_instance_subnetwork.name
+
+    // include the following to get a public IP address
+    ipv6_access_config {
+      network_tier = "PREMIUM"
+    }
   }
 
   boot_disk {
@@ -46,6 +69,7 @@ resource "google_compute_instance" "wordpress_instance_us" {
   name         = "wordpress-instance-us"
   machine_type = "e2-small"
   zone         = "us-central1-a"
+  tags         = ["web"]
 
   network_interface {
     subnetwork = google_compute_subnetwork.main_instance_subnetwork.name
@@ -59,7 +83,7 @@ resource "google_compute_instance" "wordpress_instance_us" {
 }
 
 
-# cloud sql stuff
+// cloud sql stuff
 resource "google_sql_database_instance" "db_instance" {
   database_version = "POSTGRES_14"
   region           = "us-central1"
