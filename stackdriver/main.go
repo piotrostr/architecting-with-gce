838c6b82ffd1c588d3ce627f0709eec05bee24da
Piotr Ostrowski <piotrostr@google.com>
2022-08-15 10:59:01 +0100

    feat: add error to see logs

--- stackdriver/main.go
+++ stackdriver/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "hello world\n")
+		panic("this error is to see logs")
 	})
 	http.ListenAndServe(":8080", handler)
 }
